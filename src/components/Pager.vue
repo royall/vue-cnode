2a722a34639368b1bd1185cfcee39a9a71b12759
Yangz <yangz@yangzblog.com>
2017-12-08 15:49:05 +0800

    pager

--- src/components/Pager.vue
+++ src/components/Pager.vue
@@ -7,7 +7,14 @@
           <span aria-hidden="true">&laquo;</span>
         </router-link>
       </li>
-      <router-link exact-active-class="active" tag="li" v-for="p in pageObj" :to="{name:name,params:{tab:tab},query:{page:p.page}}" :key="p.page"><a>{{p.page}}</a></router-link>
+      <li v-if="startPage>1">
+        <span>...</span>
+      </li>
+      <router-link exact-active-class="active" tag="li" v-for="p in pageObj" :to="{name:name,params:{tab:tab},query:{page:p.page}}" :key="p.page">
+        <a>{{p.page}}</a></router-link>
+      <li>
+        <span>...</span>
+      </li>
       <li>
         <router-link :to="{name:name,params:{tab:tab},query:{page:page+1}}" aria-label="Next">
           <span aria-hidden="true">&raquo;</span>
@@ -20,28 +27,28 @@
 <script>
   export default {
     name: 'Pager',
-    props:{
-      page:Number,
-      tab:String,
-      name:String
+    props: {
+      page: Number,
+      tab: String,
+      name: String
     },
-    computed:{
-      pageObj(){
-        let startPage=this.startPage,
-            endPage=this.endPage,
-            pageObj=[];
-        for(let i=startPage;i<=endPage;i++){
+    computed: {
+      pageObj() {
+        let startPage = this.startPage,
+          endPage = this.endPage,
+          pageObj = [];
+        for (let i = startPage; i <= endPage; i++) {
           pageObj.push({
-            page:i
+            page: i
           })
         }
         return pageObj;
       },
-      startPage(){
-        return this.page>3?this.page-2:1;
+      startPage() {
+        return this.page > 3 ? this.page - 2 : 1;
       },
-      endPage(){
-        return this.page>3?this.page+2:5
+      endPage() {
+        return this.page > 3 ? this.page + 2 : 5
       }
     }
   }
